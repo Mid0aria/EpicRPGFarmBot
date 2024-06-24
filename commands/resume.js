09f021e7a33e1a43d2ff09c5f0ef3ea8f312023e
Mid0aria <infoihsandemir@gmail.com>
2024-06-24 21:08:00 +0300

    fixed resume

--- commands/resume.js
+++ commands/resume.js
@@ -1,233 +1,197 @@
-var midoq = midof,
-    midoE = midof,
-    midoV = midof;
-function midoz() {
-    var h = [
-        "ils/f",
-        "7mFZfkR",
-        "delet",
-        "name",
-        "updat",
-        "expor",
-        "eady\x20",
-        "2754008SlMfMP",
-        "Bot\x20i",
-        "../ut",
-        "rpc",
-        "4235226nckaSc",
-        "hadet",
-        "d\x20hav",
-        "conte",
-        "captc",
-        "581702yNWhdR",
-        "qdLbB",
-        "WMkUq",
-        "tarte",
-        "JvUrU",
-        "chann",
-        "|4|3|",
-        "split",
-        "567192VVGYGa",
-        "BOT\x20s",
-        "s\x20alr",
-        "send",
-        "3140044cCSZQg",
-        "716405vfnlft",
-        "ng!!!",
-        "arm.j",
-        "start",
-        "15192XeVtgB",
-        "pause",
-        "globa",
-        "OgpuJ",
-        "5aVOenK",
-        "ected",
-        "1|2|0",
-        "kifRd",
-        "e\x20fun",
-        "\x20;)",
-        "cmghr",
-        "worki",
-        "KvTUg",
-    ];
-    midoz = function () {
-        return h;
-    };
-    return midoz();
+function midoL(i, L) {
+    var e = midoi();
+    return (
+        (midoL = function (m, A) {
+            m = m - (-0x321 * 0xc + -0x11 * -0x55 + 0x21ad);
+            var X = e[m];
+            return X;
+        }),
+        midoL(i, L)
+    );
 }
-(function (z, f) {
-    var midoD = {
-            z: "0x13d",
-            f: 0x130,
-            c: 0x138,
-            m: 0x13c,
-            A: 0x145,
-            d: "0x12b",
-            L: "0x121",
-            X: 0x127,
-            y: 0x141,
+var midok = midoL,
+    midou = midoL,
+    midoI = midoL;
+function midoi() {
+    var K = [
+        "globa",
+        "hadet",
+        "CMwGU",
+        "s\x20alr",
+        "expor",
+        "2682315FbRpAH",
+        "send",
+        "worki",
+        "Resum",
+        "conte",
+        "updat",
+        "chann",
+        "captc",
+        "44331NxslHi",
+        "709682cacRco",
+        "ng!!!",
+        "rpc",
+        "name",
+        "pause",
+        "1310960TZtoIs",
+        "qyACE",
+        "22131HJxQbM",
+        "|0|1",
+        "cKClI",
+        "3|2|4",
+        "eady\x20",
+        "1726686yyWyQc",
+        "split",
+        "349290hltFEP",
+        "Bot\x20i",
+        "resum",
+        "ected",
+        "ing\x20:",
+        "1712WJeCmg",
+        "168eUTYFJ",
+        "SdlYE",
+        "delet",
+    ];
+    midoi = function () {
+        return K;
+    };
+    return midoi();
+}
+(function (i, L) {
+    var midoG = {
+            i: "0x1e7",
+            L: 0x1d0,
+            e: 0x1ce,
+            m: "0x1c7",
+            A: "0x1de",
+            X: "0x1d6",
+            v: 0x1e6,
+            C: "0x1d5",
+            R: "0x1c9",
         },
-        K = midof,
-        M = midof,
-        s = midof,
-        Q = midof,
-        w = midof,
-        c = z();
+        h = midoL,
+        n = midoL,
+        B = midoL,
+        Y = midoL,
+        J = midoL,
+        e = i();
     while (!![]) {
         try {
             var m =
-                -parseInt(K(midoD.z)) / (-0x1b2 * -0x17 + 0x1dc6 + -0x44c3) +
-                parseInt(K(midoD.f)) / (0x952 * 0x1 + -0x2 * 0xb1e + 0xcec) +
-                -parseInt(s(midoD.c)) / (0xf52 + 0x146a + 0x725 * -0x5) +
-                (-parseInt(s(midoD.m)) / (0x1262 + 0x1373 + 0x1 * -0x25d1)) *
-                    (-parseInt(M(midoD.A)) /
-                        (0x1 * -0xff4 + -0x19b6 + 0x29af)) +
-                parseInt(Q(midoD.d)) /
-                    (0x4 * -0x2cf + -0x11 * -0x1eb + 0x1 * -0x1559) +
-                (-parseInt(Q(midoD.L)) / (-0x1 * 0x512 + -0x1586 + 0x1a9f)) *
-                    (parseInt(w(midoD.X)) /
-                        (0x5 * 0x7d + -0x87d * -0x3 + -0x1be0)) +
-                parseInt(K(midoD.y)) / (-0x660 * -0x4 + -0x870 + 0x1 * -0x1107);
-            if (m === f) break;
-            else c["push"](c["shift"]());
+                parseInt(h(midoG.i)) /
+                    (-0x1 * -0x19b1 + 0x1 * -0x4a7 + -0x1509) +
+                parseInt(n(midoG.L)) / (0x13 + 0x24c6 + -0x24d7) +
+                -parseInt(B(midoG.e)) / (-0x1f1c + 0x1092 + -0x2e9 * -0x5) +
+                parseInt(n(midoG.m)) /
+                    (0x14d * -0xb + 0xcb5 * -0x1 + -0x8 * -0x361) +
+                -parseInt(Y(midoG.A)) / (0x16c3 + -0xbb0 + -0x1 * 0xb0e) +
+                (-parseInt(Y(midoG.X)) / (-0x2047 + -0x1 * 0x252d + 0x457a)) *
+                    (parseInt(J(midoG.v)) /
+                        (-0x5 * -0x1d8 + -0x162d * -0x1 + 0xfaf * -0x2)) +
+                (parseInt(J(midoG.C)) / (0x21a2 + -0x79 * 0x4f + 0x3bd)) *
+                    (parseInt(B(midoG.R)) /
+                        (0x1 * 0x1a4d + -0x4 * 0x791 + 0x400));
+            if (m === L) break;
+            else e["push"](e["shift"]());
         } catch (A) {
-            c["push"](c["shift"]());
+            e["push"](e["shift"]());
         }
     }
-})(midoz, -0x209 * 0xdf + 0xd529b + -0x1 * 0x367ff);
-var midoL = {};
-function midof(z, f) {
-    var c = midoz();
-    return (
-        (midof = function (m, A) {
-            m = m - (-0x771 * -0x5 + -0x1 * -0x66c + 0x2 * -0x1541);
-            var d = c[m];
-            return d;
-        }),
-        midof(z, f)
-    );
-}
-(midoL[midoq("0x123")] = midoE(0x140)),
-    (module[midoq(0x125) + "ts"] = {
-        config: midoL,
-        run: async (c, m, A) => {
-            var midoW = {
-                    z: 0x129,
-                    f: 0x120,
-                    c: "0x13f",
-                    m: 0x147,
-                    A: "0x136",
-                    d: "0x124",
-                    L: "0x139",
-                    X: 0x133,
-                    y: "0x12d",
-                    T: 0x149,
-                    b: "0x14a",
-                    Y: 0x128,
-                    I: "0x13a",
-                    q: 0x126,
-                    E: 0x14c,
-                    V: "0x13e",
-                    D: "0x143",
-                    Z: "0x142",
-                    p: "0x132",
-                    C: 0x137,
-                    W: 0x12a,
-                    h: 0x148,
-                    S: 0x143,
-                    g: 0x12f,
-                    P: 0x12c,
-                    v: 0x146,
-                    J: 0x12f,
-                    O: "0x12c",
-                    H: "0x146",
-                    U: 0x142,
-                    R: 0x12e,
-                    l: 0x14b,
-                    a: 0x135,
-                    j: 0x13b,
-                    F: "0x122",
-                    N: "0x11f",
-                    G: "0x122",
-                    t: 0x131,
-                    i: "0x135",
-                    n: 0x13b,
+})(midoi, -0xa7 * -0x88f + -0x1 * -0xa6c4e + -0x1af1 * 0x57);
+var midoe = {};
+(midoe[midok("0x1ea")] = midou("0x1d2") + "e"),
+    (module[midok(0x1dd) + "ts"] = {
+        config: midoe,
+        run: async (m, A, X) => {
+            var midoZ = {
+                    i: 0x1c8,
+                    L: 0x1cc,
+                    e: 0x1ca,
+                    m: "0x1db",
+                    A: "0x1e1",
+                    X: 0x1d4,
+                    v: 0x1d7,
+                    C: "0x1e3",
+                    R: "0x1cb",
+                    Q: 0x1d1,
+                    d: 0x1dc,
+                    w: "0x1cd",
+                    k: 0x1e0,
+                    u: 0x1e8,
+                    I: 0x1d9,
+                    G: 0x1c6,
+                    Z: 0x1c8,
+                    K: "0x1cf",
+                    o: "0x1d8",
+                    O: "0x1e2",
+                    D: "0x1db",
+                    g: "0x1e4",
+                    y: 0x1df,
+                    s: "0x1d9",
+                    N: "0x1c6",
+                    z: "0x1e5",
+                    j: 0x1da,
+                    W: "0x1d3",
+                    V: 0x1d9,
+                    l: "0x1da",
+                    r: 0x1d3,
+                    T: 0x1e9,
+                    t: 0x1d7,
+                    S: 0x1d8,
+                    U: 0x1e2,
+                    E: "0x1df",
                 },
-                midoC = { z: "0x144", f: "0x134" },
-                o = midoq,
-                B = midoV,
-                e = midoE,
-                k = midoE,
-                x = midoq,
-                d = {
-                    OgpuJ: function (b, Y) {
-                        return b(Y);
-                    },
-                    JvUrU: o(midoW.z) + B(midoW.f) + o(midoW.c) + "s",
-                    WMkUq: e(midoW.m) + o(midoW.A) + "5",
-                    kifRd: e(midoW.d) + "e",
-                    cmghr:
-                        x(midoW.L) +
-                        x(midoW.X) +
-                        e(midoW.y) +
-                        k(midoW.T) +
-                        e(midoW.b),
-                    KvTUg: function (b, Y, I) {
-                        return b(Y, I);
-                    },
-                    qdLbB:
-                        e(midoW.Y) +
-                        e(midoW.I) +
-                        k(midoW.q) +
-                        B(midoW.E) +
-                        x(midoW.V),
-                };
-            if (c[k(midoW.D) + "l"][x(midoW.Z) + "d"]) {
-                var L = d[o(midoW.p)][x(midoW.C)]("|"),
-                    X = -0x1675 + -0xa52 * -0x2 + 0x1d1;
+                q = midoI,
+                F = midou,
+                M = midou,
+                f = midok,
+                H = midok,
+                v = {};
+            (v[q(midoZ.i)] = q(midoZ.L) + M(midoZ.e)),
+                (v[F(midoZ.m)] = H(midoZ.A) + F(midoZ.X) + ")"),
+                (v[F(midoZ.v)] = M(midoZ.C) + "e"),
+                (v[f(midoZ.R)] =
+                    f(midoZ.Q) +
+                    f(midoZ.d) +
+                    M(midoZ.w) +
+                    f(midoZ.k) +
+                    q(midoZ.u));
+            var C = v;
+            if (m[H(midoZ.I) + "l"][M(midoZ.G) + "d"]) {
+                var R = C[F(midoZ.Z)][f(midoZ.K)]("|"),
+                    Q = -0x2034 + 0x2ce + 0x8e * 0x35;
                 while (!![]) {
-                    switch (L[X++]) {
+                    switch (R[Q++]) {
                         case "0":
-                            c[e(midoW.W)](d[k(midoW.h)]);
+                            await A[H(midoZ.o) + "e"]();
                             continue;
                         case "1":
-                            c[x(midoW.S) + "l"][
-                                B(midoW.g) + o(midoW.P) + o(midoW.v)
-                            ] &&
-                                (c[e(midoW.D) + "l"][
-                                    B(midoW.J) + B(midoW.O) + k(midoW.H)
-                                ] = ![]);
+                            var d = {};
+                            (d[H(midoZ.O) + "nt"] = C[H(midoZ.D)]),
+                                await A[f(midoZ.g) + "el"][H(midoZ.y)](d);
                             continue;
                         case "2":
-                            c[e(midoW.D) + "l"][x(midoW.U) + "d"] = ![];
+                            m[M(midoZ.s) + "l"][F(midoZ.N) + "d"] = ![];
                             continue;
                         case "3":
-                            var y = {};
-                            (y[o(midoW.R) + "nt"] = d[x(midoW.l)]),
-                                await m[B(midoW.a) + "el"][B(midoW.j)](y);
+                            m[M(midoZ.s) + "l"][
+                                F(midoZ.z) + F(midoZ.j) + F(midoZ.W)
+                            ] &&
+                                (m[M(midoZ.V) + "l"][
+                                    F(midoZ.z) + M(midoZ.l) + q(midoZ.r)
+                                ] = ![]);
                             continue;
                         case "4":
-                            await m[x(midoW.F) + "e"]();
-                            continue;
-                        case "5":
-                            d[x(midoW.N)](
-                                setTimeout,
-                                () => {
-                                    var r = e,
-                                        u = x;
-                                    d[r(midoC.z)](require, d[r(midoC.f)])(c, m);
-                                },
-                                0x8d5 + 0x1 * 0x139f + -0x188c
-                            );
+                            m[F(midoZ.T)](C[H(midoZ.t)]);
                             continue;
                     }
                     break;
                 }
             } else {
-                await m[x(midoW.G) + "e"]();
-                var T = {};
-                (T[B(midoW.R) + "nt"] = d[k(midoW.t)]),
-                    await m[e(midoW.i) + "el"][o(midoW.n)](T);
+                await A[f(midoZ.S) + "e"]();
+                var w = {};
+                (w[F(midoZ.U) + "nt"] = C[f(midoZ.R)]),
+                    await A[M(midoZ.g) + "el"][H(midoZ.E)](w);
             }
         },
     });
